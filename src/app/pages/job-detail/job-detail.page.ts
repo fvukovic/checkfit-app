af0d8364706a30dfda2ec9da1f86ea9e8a94645c
mpopijac <matija.popijac@hotmail.com>
2020-10-24 00:08:28 +0200

    Job detail page, details are added

--- src/app/pages/job-detail/job-detail.page.ts
+++ src/app/pages/job-detail/job-detail.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-job-detail',
@@ -6,19 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./job-detail.page.scss'],
 })
 export class JobDetailPage implements OnInit {
-
   slideOptsOne = {
     initialSlide: 0,
     slidesPerView: 1,
-    autoplay:true,
+    autoplay: true,
     speed: 2000,
-   };
+  };
 
-  constructor() { }
+  constructor(public translate: TranslateService) {}
 
-  ngOnInit() {
-  }
-
- 
-
+  ngOnInit() {}
 }
